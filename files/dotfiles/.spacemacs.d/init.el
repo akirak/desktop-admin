3ffca63613040e1b5a5fb5c49f7531c976f92d7c
Akira Komamura <akira.komamura@gmail.com>
2017-04-03 16:03:26 +0800

    Spacemacs: Remove org-journal layer

--- files/dotfiles/.spacemacs.d/init.el
+++ files/dotfiles/.spacemacs.d/init.el
@@ -67,7 +67,6 @@ values."
      spell-checking
      ;; syntax-checking
      ;; version-control
-     journal
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
