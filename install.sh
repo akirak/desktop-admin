281edd0ed5a53c7883c02d105603d8be198535f5
Akira Komamura <akira.komamura@gmail.com>
2017-11-01 00:39:49 +0900

    Tweak the installation script for supporting Solus

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
-sudo pacman -Sy --noconfirm --asdeps ansible npm \
-  && git submodule init \
+
+# Install dependencies
+
+# Solus
+if [ -f /etc/solus-release ]; then
+    sudo eopkg install ansible nodejs
+# Arch Linux
+elif [ -f /etc/arch-release ]; then
+    sudo pacman -Sy --noconfirm --asdeps ansible npm
+else
+    echo "Unsupported system"
+    exit 1
+fi
+
+if [ $? -gt 0 ]; then
+    exit 1
+fi
+
+git submodule init \
   && npm run up
