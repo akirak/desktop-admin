954c8a1dfee243f2292d9328280062c58e97c4b3
Akira Komamura <akira.komamura@gmail.com>
2018-01-01 16:07:06 +0900

    Rename Docker tags

--- test-archlinux.mk
+++ test-archlinux.mk
@@ -1,17 +1,17 @@
-ARCH_DOCKER_TAG = akirak-arch
+ARCH_DOCKER_TAG = akirak/linux-playbook:arch
 
 all: user graphical
 
 user: init
-			docker run $(ARCH_DOCKER_TAG):init
+			docker run $(ARCH_DOCKER_TAG)-init
 
 graphical: init
 					docker run -u root \
-						$(ARCH_DOCKER_TAG):init \
+						$(ARCH_DOCKER_TAG)-init \
 						ansible-playbook -c local --tags=graphical init.yml
 
 init:
-			docker build -t $(ARCH_DOCKER_TAG):init -f archlinux.dockerfile .
+			docker build -t $(ARCH_DOCKER_TAG)-init -f archlinux.dockerfile .
 
 full:
-		docker build -t $(ARCH_DOCKER_TAG):full -f archlinux-full.dockerfile .
+		docker build -t $(ARCH_DOCKER_TAG)-full -f archlinux-full.dockerfile .
